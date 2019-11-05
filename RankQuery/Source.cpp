930b4082ddfda90a02a12d72cb80c0a5fd5b5353
Zinat <zinat36@gmail.com>
2019-11-05 18:43:40 -0500

    Implemented wt build rank select

--- RankQuery/Source.cpp
+++ RankQuery/Source.cpp
@@ -466,6 +466,112 @@ wavelet_tree wavelet_tree_from_file(std::istream& fin) {
 
 }
 
+
+int main(int argc, char** argv) 
+{ 
+	if(argc<3){
+		cout<< "Not al parameters given"<<endl;
+		return 1;
+	}
+
+    std::string arg = argv[1];
+	if(arg=="build") {
+		//$wt build <input string> <output file>
+		std::string input = argv[2];
+		std::string output = argv[3];
+
+		ifstream infile (input);
+		ofstream outfile (output);
+
+		string bits;
+  	
+		if (infile.is_open())
+		{
+			
+			getline (infile,bits);
+			cout << bits << '\n';
+			wavelet_tree wt(bits);
+			if(outfile.is_open()){
+				wavelet_tree_dump(outfile, &wt);
+				outfile.close();
+			}
+			infile.close();
+			cout<<"Number of distinct input characters:\t"<<wt.alphabet.size()<<"\n";
+			cout<<"Number of characters in the input string:\t"<<bits.size()<<"\n";
+
+		}
+	}
+
+	if(arg=="access"){
+		std::string input = argv[2];
+		std::string indices = argv[3];
+
+		ifstream infile (input);
+		ifstream queryindices (indices);
+		if (infile.is_open())
+		{
+			wavelet_tree wt1 = wavelet_tree_from_file(infile);
+			infile.close();
+			int a;
+			while (queryindices >> a)
+			{
+				cout<<a<<"\n";
+				// process pair (a,b)
+			}
+		}
+
+
+
+	}
+
+
+
+	if(arg=="rank"){
+		std::string input = argv[2];
+		std::string indices = argv[3];
+
+		ifstream infile (input);
+		ifstream queryindices (indices);
+		if (infile.is_open())
+		{
+			wavelet_tree wt1 = wavelet_tree_from_file(infile);
+			infile.close();
+			char a;
+			int b;
+			while (queryindices >> a>>b)
+			{
+				cout<<wt1.rank(a,b)<<"\n";
+				// process pair (a,b)
+			}
+		}
+
+	}
+    
+	if(arg=="select"){
+		std::string input = argv[2];
+		std::string indices = argv[3];
+
+		ifstream infile (input);
+		ifstream queryindices (indices);
+		if (infile.is_open())
+		{
+			wavelet_tree wt1 = wavelet_tree_from_file(infile);
+			infile.close();
+			char a;
+			int b;
+			while (queryindices >> a>>b)
+			{
+				cout<<wt1.select(a,b)<<"\n";
+				// process pair (a,b)
+			}
+		}
+
+	}
+    return 0; 
+} 
+
+/*
+
 int main(void) {
 	//string bits;
 	//ifstream infile("input.txt");
@@ -501,7 +607,7 @@ int main(void) {
 	}
 
 	else cout << "Unable to open file";
-	*/
+	
 
 	//std::string bits("10101011101011111000");
 
@@ -515,7 +621,7 @@ int main(void) {
 	ifstream inputfile("output.txt");
 
 	wavelet_tree wt1 = wavelet_tree_from_file(inputfile);
-	/*
+	
 	std::string bits("1001011100001010");
 
 	//bit_vector b(bits);
@@ -541,7 +647,8 @@ int main(void) {
 	std::vector<bool >::reference ref1 = bit_vector.at(0);
 	cout << ref1 << endl; // ref1 implicitly cast to bool
 
-	*/
+	
 
 	return 0;
 }
+*/
