584440542240e075fba5fcccb76f38a881b86bfe
Zinat <zinat36@gmail.com>
2019-11-06 23:41:12 -0500

    changed script

--- RankQuery/script.sh
+++ RankQuery/script.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
-g++ -o rank2 Source.cpp #-i headers -I folders 
-./rank2 $1 $2 $3
+#g++ -o rank2 Source.cpp #-i headers -I folders 
+g++ -std=c++11 Source.cpp Wavelet/wavelet.cpp Select/Select.cpp Rank/Rank.cpp -o src -D_GLIBCXX_USE_CXX11_ABI=1
+./src $1 $2 $3
 #echo "$#"
